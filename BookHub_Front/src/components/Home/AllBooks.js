73a2b04b1c5519a1af8ee8e03cde541f3d173dba
AhmedSoror <ahmedsoror438@gmail.com>
2019-12-09 01:27:10 +0200

    allbooks list done

--- BookHub_Front/src/components/Home/AllBooks.js
+++ BookHub_Front/src/components/Home/AllBooks.js
@@ -1,51 +1,164 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { StyleSheet, Text, View, List, ListItem } from "react-native";
-import { Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button
+} from "native-base";
+import Collapsible from "react-native-collapsible";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
+import CircleButton from "react-native-circle-button";
 import axios from "axios";
 axios.defaults.baseURL = "http://172.17.0.2:3000/";
 
-import AllBooksCard from "./AllBooksCard";
+import AddBook from "./AddBook";
+import ReservationDetails from "./ReservationDetails";
 
-class HomeScreen extends Component {
+class AllBooks extends Component {
   static navigationOptions = {
-    title: "All books"
+    title: "My books"
   };
-
   constructor(props) {
     super(props);
     this.state = {
-      allBookList: null
+      loading: true,
+      bookInfoCollapsed: true,
+      addBookVisible: false,
+      reservationVisible: false,
+      bookSelected: null,
+      bookSelectedBorrower: null,
+      bookList: null
     };
   }
-
-  componentDidMount() {
-    axios
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("../../../node_modules/native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("../../../node_modules/native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ isReady: true });
+    this.fetchBooks();
+  }
+  async fetchBooks() {
+    await axios
       .get("/books")
       .then(response => {
         this.setState({
-          allBookList: response.data
+          bookList: response.data
         });
       })
-
       .catch(error => {
         console.log(`all books error: ${error}`);
       });
+
+    var newList = this.state.bookList;
+    flag = 0;
+    for (i = 0; i < newList.length; i++) {
+      bookItem = newList[i];
+      await axios
+        // .get(`/users/${bookItem.borrower_id.$oid}`)
+        .get(`/users/${bookItem.owner_id.$oid}`)
+        .then(response => {
+          bookItem.owner = response.data;
+          bookItem.isCollapsed = true;
+          newList[i] = bookItem;
+        })
+        .catch(error => {
+          console.log(`AllBooks L135 error: ${error}`);
+        });
+      if (i == newList.length - 1) {
+        flag = 1;
+      }
+    }
+    if (flag == 1) {
+      this.setState({
+        bookList: newList
+      });
+    }
   }
 
   render() {
-    if (this.state.allBookList) {
-      const bookCards = this.state.allBookList.map(item => {
-        return <AllBooksCard book={item} key={item._id.$oid} />;
-      });
-      return <View style={styles.container}>{bookCards}</View>;
-    } else {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+    if (this.state.bookList) {
       return (
-        <View>
-          <Text>{"Loading ...."}</Text>
-        </View>
+        <Container>
+          <Content>
+            <List
+              dataArray={this.state.bookList}
+              renderRow={book => (
+                <ListItem thumbnail>
+                  <Left>
+                    <Image
+                      source={require("../../../assets/book.png")}
+                      style={styles.ImageIconStyle}
+                    />
+                  </Left>
+                  <Body>
+                    <TouchableOpacity
+                      onPress={async () => {
+                        newBooks = this.state.bookList;
+                        for (i = 0; i < newBooks.length; i++) {
+                          if (newBooks[i]._id.$oid == book._id.$oid) {
+                            newBooks[i].isCollapsed = !newBooks[i].isCollapsed;
+                          }
+                        }
+                        await this.setState({ bookList: newBooks });
+                      }}
+                    >
+                      <Text>{book.title}</Text>
+                      <Text note numberOfLines={3}>
+                        {book.reserved ? "Reserved" : "Available"}
+                      </Text>
+                    </TouchableOpacity>
+                    <Collapsible
+                      collapsed={
+                        book.isCollapsed == undefined ? true : book.isCollapsed
+                      }
+                    >
+                      <Text>
+                        {book.owner ? `${book.owner.name}` : "loading..."}
+                      </Text>
+                    </Collapsible>
+                  </Body>
+                  
+                </ListItem>
+              )}
+              keyExtractor={(book, index) => index.toString()}
+            ></List>
+          </Content>
+          <View
+            style={{ flex: 1, position: "absolute", bottom: "2%", right: "2%" }}
+          >
+            <CircleButton
+              size={45}
+              iconButtonCenter={require("../../../assets/circle.png")}
+              iconButtonLeft={require("../../../assets/refresh.png")}
+              iconButtonRight={require("../../../assets/add.png")}
+              iconButtonBottom={require("../../../assets/logout.png")}
+              // iconButtonTop={require("../../../assets/icon.png")}
+
+              onPressButtonLeft={() => {
+                this.fetchBooks();
+              }}
+              onPressButtonBottom={() => {
+                this.props.navigation.navigate("Login");
+              }}
+            />
+          </View>
+        </Container>
       );
+    } else {
+      return <Text>{"Loading ...."}</Text>;
     }
   }
 }
@@ -57,6 +170,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "white",
     marginTop: 20
+  },
+  gridView: {
+    marginTop: 20,
+    borderWidth: 3,
+    borderRadius: 15,
+    padding: 5,
+    width: "90%",
+    alignSelf: "center"
+  },
+  ImageIconStyle: {
+    padding: 2,
+    margin: 2,
+    height: 30,
+    width: 30
   }
 });
 
@@ -69,4 +196,4 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {};
 };
-export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(AllBooks);
